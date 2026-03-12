6422c4d18325644fd34dbf80eefee7f3368a4eab
dm-carmo <davidcarmo.1997@gmail.com>
2026-03-12 16:02:52 +0000

    Done some work - contains a landing page and install/changes info

--- index.js
+++ index.js
@@ -1,40 +1,63 @@
 // Call updateContent() on page load
 window.addEventListener("DOMContentLoaded", async () => {
-  const userPreferredLanguage = localStorage.getItem("lang") || "en";
-  const langData = await fetchLanguageData(userPreferredLanguage);
-  updateContent(langData);
-
-  const langBtn = document.querySelector("#langBtn");
-  langBtn.setAttribute("class", userPreferredLanguage);
+  todaysDate();
 });
 
-// Function to update content based on selected language
-function updateContent(langData) {
-  document.querySelectorAll("[data-i18n]").forEach((element) => {
-    const key = element.getAttribute("data-i18n");
-    element.innerHTML = langData[key];
-  });
+function todaysDate() {
+  // Create a new Date object
+  const currentDateTime = new Date();
+
+  // Extract the date and time components
+  const time = currentDateTime.getHours();
+
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayOfWeek = daysOfWeek[currentDateTime.getDay()];
+  const day = currentDateTime.getDate();
+  const month = currentDateTime.getMonth();
+  const year = currentDateTime.getFullYear().toString().substr(-2);
+  const date =
+    day + "." + month + "." + year + " " + (time >= 12 ? "PM" : "AM");
+
+  const dateDisplayElement = document.getElementById("date");
+  const timeDisplayElement = document.getElementById("time");
+
+  // Display the date and time in the span element
+  dateDisplayElement.textContent = `${dayOfWeek}`;
+  timeDisplayElement.textContent = `${date}`;
 }
 
-// Function to set the language preference
-function setLanguagePreference(lang) {
-  localStorage.setItem("lang", lang);
-  location.reload();
+function showHideCompsTable() {
+  const compsTableElement = document.getElementById("comps-table");
+  if (compsTableElement.style.visibility == "visible")
+    compsTableElement.style.visibility = "hidden";
+  else compsTableElement.style.visibility = "visible";
 }
 
-// Function to fetch language data
-async function fetchLanguageData(lang) {
-  const response = await fetch(`lang/${lang}.json`);
-  return response.json();
+function setHomeText() {
+  const homeText =
+    "Welcome to the overview on the 2025/26 restructure patch, created by davdav. Here you'll be able to download the patch and read detailed info about what's been changed, along with some preview images.";
+  const compsTableElement = document.getElementById("content-text");
+  compsTableElement.innerHTML = homeText;
 }
 
-// Function to change language
-async function changeLanguage(lang) {
-  await setLanguagePreference(lang);
+function setDownloadText() {
+  $("#content-text").load("download.html");
+}
 
-  const langData = await fetchLanguageData(lang);
-  updateContent(langData);
+function setChangesText() {
+  $("#content-text").load("changelog.html");
+}
 
-  const langBtn = document.querySelector("#langBtn");
-  langBtn.setAttribute("class", lang);
+function setPreviewsText() {
+  const homeText = "Previews here";
+  const compsTableElement = document.getElementById("content-text");
+  compsTableElement.innerHTML = homeText;
 }
